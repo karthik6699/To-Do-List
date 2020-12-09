17fa914c179868c66c7e6cf794de5ecdda57493f
karthik6699 <kindkarthik007@gmail.com>
2020-12-09 18:18:23 +0530

    proc

--- ToDoApp/ToDoApp/settings.py
+++ ToDoApp/ToDoApp/settings.py
@@ -24,9 +24,9 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'jyga61a)!saq6!w$iycb$o%9-5%j8k%xsx-n%0xmc(*jvh$l8-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0. 0.1.','todoapp-django1.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1.','todoapp-django1.herokuapp.com']
 X_FRAME_OPTIONS = 'ALLOW ALL'
 
 # Application definition
